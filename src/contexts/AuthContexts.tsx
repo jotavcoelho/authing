bbbc850929b8c882618e3973ab6fa78c14b9462e
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-07-12 20:29:25 -0300

    added axios, connecting to api

--- src/contexts/AuthContexts.tsx
+++ src/contexts/AuthContexts.tsx
@@ -1,5 +1,6 @@
 import { Sign } from "crypto";
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 type SignInCredentials = {
   email: string;
@@ -21,7 +22,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const isAuthenticated = false;
 
   async function signIn({ email, password }: SignInCredentials) {
-    console.log({ email, password });
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password
+      });
+  
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);      
+    }
   }
 
   return (
